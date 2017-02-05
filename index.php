06b813a06a41fd3f0cdd0f044e4a41d8cc103421
kgurushankar <kgurushankar@users.noreply.github.com>
2017-02-05 04:52:40 -0800

    Upload php

---
+++ index.php
@@ -0,0 +1,17 @@
+<DOCTYPE html>
+<html>
+
+
+<script src="https://www.gstatic.com/firebasejs/3.6.2/firebase.js"></script>
+<script src="https://www.gstatic.com/firebasejs/3.6.2/firebase-app.js"></script>
+<script src="https://www.gstatic.com/firebasejs/3.6.2/firebase-database.js"></script>
+<script src="firebase access.js"></script>
+<script src="main.js"></script>
+
+<?php
+	echo ('<script type="text/javascript">messageHandler('+$GET['userin']+','+$GET['step']+');</script>')
+
+}
+
+?>
+</html>
